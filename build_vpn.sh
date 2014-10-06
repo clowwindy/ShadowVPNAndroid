00512e0d8733d9cab3059273160da4fca7c73e3e
clowwindy <clowwindy42@gmail.com>
2014-10-07 00:00:48 +0800

    update script

--- build_vpn.sh
+++ build_vpn.sh
@@ -6,7 +6,7 @@ if [ a == a$ANDROID_NDK_HOME ]; then
 fi
 
 pushd jni/ShadowVPN || exit 1
-./autogen.sh || exit 1
+#./autogen.sh || exit 1
 
 if [ ! -f shadowvpn-android-arm/lib/libshadowvpn.a ]; then
     dist-build/android-arm.sh || exit 1
